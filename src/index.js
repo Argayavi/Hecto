3689632735cc144514349b5211460fd3b8ca8bb0
unknown <argayavi@gmail.com>
2022-09-13 17:24:43 +0530

    newchange
    newchange

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Header from './Header';
-// import Discountitem from './Discountitem';
-import Latestblog from './Latestblog';
+import Discountitem from './Discountitem';
+// import Latestblog from './Latestblog';
 // import Subbanner from './Subbanner';
 // import GridView from './GridView';
 // import BrandList from './BrandList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Latestblog/>
+    <Discountitem/>
   </React.StrictMode>
 );
 
