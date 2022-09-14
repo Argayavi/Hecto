ab013991c023451276a7b076d747df411f451ff1
unknown <argayavi@gmail.com>
2022-09-14 17:15:24 +0530

    login page
    login page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Header from './Header';
-import Discountitem from './Discountitem';
+// import Discountitem from './Discountitem';
 // import Latestblog from './Latestblog';
 // import Subbanner from './Subbanner';
 // import GridView from './GridView';
 // import BrandList from './BrandList';
-// import Login from './Login';
+import Login from './Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Discountitem/>
+    <Login/>
   </React.StrictMode>
 );
 
